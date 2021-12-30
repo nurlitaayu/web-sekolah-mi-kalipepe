7abc5db93f5929cf4ecf7b99cdadda935741167b
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2021-12-30 16:45:48 +0700

    Create Datamapel.php

---
+++ application/controllers/Datamapel.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Datamapel extends CI_Controller {
+
+	public function index()	{
+		$this->load->view('admin/v_datamapel');
+	}
+}
