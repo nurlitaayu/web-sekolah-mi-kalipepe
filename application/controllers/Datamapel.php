e7b9fa8596f911a7d05a2c61822dfa5a136cc1a3
nurlitaayu <nurlitaayu62638@gmail.com>
2021-12-30 20:10:25 +0700

    datamapel
    mata pelajaran

--- application/controllers/Datamapel.php
+++ application/controllers/Datamapel.php
@@ -3,7 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Datamapel extends CI_Controller {
 
-	public function index()	{
-		$this->load->view('admin/v_datamapel');
+	public function index()
+	{
+		$data = array(
+			'title'	=> 'mi muhammadiyah kalipepe',
+			'title2'=> 'Mata Pelajaran',
+			'isi'	=> 'admin/v_mapel'		
+			);
+		$this->load->view('admin/v_mapel');
 	}
 }
