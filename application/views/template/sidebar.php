3a82836c19fad64dcc6adf8ad17dc3ac40a0397d
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2021-12-30 14:19:28 +0700

    Update sidebar.php

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -21,32 +21,9 @@
           </li>
           <li class="nav-header">EXAMPLES</li>
           <li class="nav-item">
-            <a href="pages/calendar.html" class="nav-link">
-              <i class="nav-icon far fa-calendar-alt"></i>
-              <p>
-                Calendar
-                <span class="badge badge-info right">2</span>
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="<?php echo base_url('Dataguru'); ?>" class="nav-link">
+            <a href="<?php echo base_url('dataguru'); ?>" class="nav-link">
               <i class="nav-icon far fa-user"></i>
               <p>Data Guru</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="pages/gallery.html" class="nav-link">
-              <i class="nav-icon far fa-image"></i>
-              <p>Gallery</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="pages/kanban.html" class="nav-link">
-              <i class="nav-icon fas fa-columns"></i>
-              <p>
-                Kanban Board
-              </p>
             </a>
           </li>
           <li class="nav-item">
