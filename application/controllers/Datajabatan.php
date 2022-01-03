730cddc9a37c6cebd16695e5743b0ca8d543b8a1
MUFID ADITYA DAFENTRI <72061725+aditdafentri@users.noreply.github.com>
2022-01-03 15:57:45 +0700

    upload dataJabatan

--- application/controllers/Datajabatan.php
+++ application/controllers/Datajabatan.php
@@ -2,8 +2,33 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class DataJabatan extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_jabatan');
+	}
 
 	public function index()	{
-		$this->load->view('admin/v_datajabatan');
+		$data = array(
+			'title'	=> 'mi muhammadiyah kalipepe',
+			'title2'=> 'data jabatan',
+			'jabatan'	=>  $this->m_jabatan->get_datajabatan('tb_jabatan')->result(),
+			'isi'	=> 'admin/v_datajabatan'		 
+		);
+		$this->load->view('admin/v_datajabatan',$data);
+	}
+
+	public function simpan(){
+		$posisi_jabatan = $this->input->post('posisi_jabatan');
+		$data = array(
+			'posisi_jabatan' => $posisi_jabatan
+		);
+		return $this->m_jabatan->save('tb_jabatan', $data);
+	}
+
+	public function delete(){
+		$id_jabatan = $this->input->post('id_jabatan');	
+		$this->m_jabatan->delete('tb_jabatan', $id_jabatan);		
 	}
 }
+
