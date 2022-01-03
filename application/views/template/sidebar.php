69611b09635ccbbd16680f25bcf4decdf31d06f0
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-04 04:26:28 +0700

    Update sidebar.php

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -39,6 +39,12 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
+                <a href="<?php echo base_url('cms/pages') ?>" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Pages</p>
+                </a>
+              </li>
+              <li class="nav-item">
                 <a href="<?php echo base_url('cms') ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Create Post</p>
