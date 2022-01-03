27d81021793984bb993a37366e91de74b7064b23
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-04 04:26:32 +0700

    Update v_listpost.php

---
+++ application/views/admin/cms/v_listpost.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta id="viewport" content="width=device-width, initial-scale=1">
+  <title>Mi Muhammadiyah Kalipepe | List Post</title>
+	<?php $this->load->view('template/head.php'); ?>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+<?php $this->load->view('template/navbar.php') ?>
+
+<?php $this->load->view('template/sidebar.php') ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">List Post</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">List Post</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+              <button class="btn btn-xs btn-primary card-title" data-toggle="modal" data-target="#tambah-guru"><i class="fas fa-plus"></i> Tambah Guru</button>
+              <div class="card-tools">
+                <div class="input-group input-group-sm" style="width: 150px;">
+                  <input type="text" id="table_search" class="form-control float-right" placeholder="Search">
+
+                  <div class="input-group-append">
+                    <button type="submit" class="btn btn-default">
+                      <i class="fas fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body table-responsive p-0 text-center" style="height: 300px;">
+              <table class="table table-head-fixed text-nowrap table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Nama Kategori</th>
+                    <th>Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>asda</td>
+                    <td>
+                      <button class="btn btn-sm btn-warning"><i class="fas fa-edit" style="color: #fff;"></i></button>
+                      <button class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+      </div>
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+<?php $this->load->view('template/footer.php'); ?>
+<script type="text/javascript">
+   $('#reservationdate').datetimepicker({
+        format: 'L'
+    });
+</script>
+  <script>
+  $('#tombolsimpan').on('click',function(){
+    var $nama =$('#nama_guru').val();
+    var $tempatlahir =$('#tempat_lahir').val();
+    var $nip =$('#nip').val();
+    var $tgllahir =$('#tgl_lahir').val();
+    var $jabatan =$('#id_jabatan').val();
+    var $pendidikan =$('#pendidikan').val();
+    var $mapel =$('#id_mapel').val();
+    var $foto =$('#foto').val();
+
+    $.ajax({
+      url:"<?php echo site_url("dataguru/simpan")?>",
+      type:"POST",
+      success:function(hasil){
+        alert(hasil);
+      }
+    });
+  });
+  </script>
+</body>
+</html>
