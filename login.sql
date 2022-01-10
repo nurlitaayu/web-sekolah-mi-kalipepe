7e72c94389c332c2eb839f91a73c0471e00296e8
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-10 23:38:46 +0700

    Update login.sql

--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2022 at 10:04 AM
+-- Generation Time: Jan 10, 2022 at 05:37 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -31,10 +31,9 @@ CREATE TABLE `tb_guru` (
   `id_guru` int(11) NOT NULL,
   `id_jabatan` int(11) NOT NULL,
   `nip` varchar(20) NOT NULL,
-  `nama_guru` varchar(25) NOT NULL,
+  `nama_guru` varchar(50) NOT NULL,
   `tempat_lahir` varchar(15) NOT NULL,
   `tgl_lahir` date NOT NULL,
-  `id_mapel` int(2) DEFAULT NULL,
   `pendidikan` varchar(5) NOT NULL,
   `foto_guru` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -43,8 +42,10 @@ CREATE TABLE `tb_guru` (
 -- Dumping data for table `tb_guru`
 --
 
-INSERT INTO `tb_guru` (`id_guru`, `id_jabatan`, `nip`, `nama_guru`, `tempat_lahir`, `tgl_lahir`, `id_mapel`, `pendidikan`, `foto_guru`) VALUES
-(1, 1, '333', 'AHMAD JUNAEDI, SH', 'jember', '2021-12-01', NULL, 's1', '');
+INSERT INTO `tb_guru` (`id_guru`, `id_jabatan`, `nip`, `nama_guru`, `tempat_lahir`, `tgl_lahir`, `pendidikan`, `foto_guru`) VALUES
+(9, 4, '123456', 'Befril Renfiliani Syafitri', 'Jember', '2000-12-01', 'D3 ', 'bef.jpg'),
+(12, 3, '123', 'asd', 'Lumajang', '2022-01-03', 'SMK', '20201111_113225.jpg'),
+(13, 7, '123123', 'Khusnul Khotimah', 'Lumajang', '0000-00-00', 'SMK', 'IMG_3687.JPG');
 
 -- --------------------------------------------------------
 
@@ -62,7 +63,13 @@ CREATE TABLE `tb_jabatan` (
 --
 
 INSERT INTO `tb_jabatan` (`id_jabatan`, `posisi_jabatan`) VALUES
-(1, 'kepala sekolah');
+(1, 'Kepala Sekolah'),
+(2, 'Bendahara'),
+(3, 'Kurikulum'),
+(4, 'Guru Pengajar'),
+(5, 'Tata Usaha'),
+(6, 'Guru Pengajar'),
+(7, 'Tata Usaha');
 
 -- --------------------------------------------------------
 
@@ -84,10 +91,7 @@ INSERT INTO `tb_kategori` (`id_kategori`, `kategori`) VALUES
 (2, 'Prestasi'),
 (3, 'galeri'),
 (4, 'asdasd'),
-(5, 'asd'),
-(6, 'qq'),
-(7, 'asdasdas'),
-(8, 'coba lagi');
+(11, 'coba lagi');
 
 -- --------------------------------------------------------
 
@@ -99,6 +103,34 @@ CREATE TABLE `tb_mapel` (
   `id_mapel` int(11) NOT NULL,
   `mata_pelajaran` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_mapel`
+--
+
+INSERT INTO `tb_mapel` (`id_mapel`, `mata_pelajaran`) VALUES
+(1, 'Tematik'),
+(2, 'Matematika'),
+(3, 'Tematik'),
+(4, 'Matematika'),
+(5, 'PJOK'),
+(6, 'Bahasa Daerah'),
+(7, 'PJOK'),
+(8, 'Bahasa Daerah'),
+(9, 'Bahasa Inggris'),
+(10, 'Bahasa Arab'),
+(11, 'Bahasa Inggris'),
+(12, 'Bahasa Arab'),
+(13, 'Fiqih'),
+(14, 'Al - Quran Hadist'),
+(15, 'Fiqih'),
+(16, 'Al - Quran Hadist'),
+(17, 'Sejarah Kebudayaan Islam'),
+(18, 'Aqidah Akhlak'),
+(19, 'Sejarah Kebudayaan Islam'),
+(20, 'Aqidah Akhlak'),
+(21, 'Kemuhammadiyahan'),
+(22, 'Kemuhammadiyahan');
 
 -- --------------------------------------------------------
 
@@ -129,7 +161,6 @@ INSERT INTO `user_login` (`id`, `user_name`, `user_password`) VALUES
 --
 ALTER TABLE `tb_guru`
   ADD PRIMARY KEY (`id_guru`),
-  ADD KEY `id_mapel` (`id_mapel`),
   ADD KEY `id_jabatan` (`id_jabatan`);
 
 --
@@ -164,25 +195,25 @@ ALTER TABLE `user_login`
 -- AUTO_INCREMENT for table `tb_guru`
 --
 ALTER TABLE `tb_guru`
-  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_guru` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tb_jabatan`
 --
 ALTER TABLE `tb_jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_kategori`
 --
 ALTER TABLE `tb_kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tb_mapel`
 --
 ALTER TABLE `tb_mapel`
-  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `user_login`
@@ -198,7 +229,6 @@ ALTER TABLE `user_login`
 -- Constraints for table `tb_guru`
 --
 ALTER TABLE `tb_guru`
-  ADD CONSTRAINT `tb_guru_ibfk_1` FOREIGN KEY (`id_mapel`) REFERENCES `tb_mapel` (`id_mapel`),
   ADD CONSTRAINT `tb_guru_ibfk_2` FOREIGN KEY (`id_jabatan`) REFERENCES `tb_jabatan` (`id_jabatan`);
 COMMIT;
 
