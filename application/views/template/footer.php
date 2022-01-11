82b8529d848c20de7a0e01b10ae9e00691327035
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-11 13:03:05 +0700

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -36,8 +36,6 @@ daterangepicker
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<!-- jquery cdn -->
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script> -->
 <script>
   $('.custom-file-input').on('change', function(){
     let fileName = $(this).val().split('\\').pop();
