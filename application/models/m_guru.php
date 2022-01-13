2a537e68ddc63cc6d6c98bc146e5ff3e31a1ae0f
ardyassetya <72063228+Ardyas-setya@users.noreply.github.com>
2022-01-14 00:19:25 +0700

    proses delete foto

--- application/models/m_guru.php
+++ application/models/m_guru.php
@@ -39,49 +39,16 @@ class M_guru extends CI_Model {
         $this->db->delete('tb_guru', array('id_guru' => $id_guru));
         unlink(FCPATH."/foto/fotoguru/".$foto_guru);
     }
-    public function edit_guru($id){
-        $foto_guru 		= $this->upload->data();
-            $foto_guru 		= $foto_guru['file_name'];
-            $nama_guru 		= $this->input->post('nama_guru', TRUE);
-            $nip 			= $this->input->post('nip', TRUE);
-            $tempat_lahir 	= $this->input->post('tempat_lahir', TRUE);
-            $tgl_lahir 		= $this->input->post('tgl_lahir', TRUE);
-            $id_jabatan 	= $this->input->post('id_jabatan', TRUE);
-            $pendidikan 	= $this->input->post('pendidikan', TRUE);
-
-            $data = array(
-            	'nama_guru' => $nama_guru,
-            	'nip' => $nip,
-            	'tempat_lahir' => $tempat_lahir,
-            	'tgl_lahir' => $tgl_lahir,
-            	'id_jabatan' => $id_jabatan,
-            	'pendidikan' => $pendidikan,
-            	'foto_guru' => $foto_guru
-            );
+    public function edit_guru($id,$data){
             $this->db->where('id_guru',$id);
+            $this->db->update('tb_guru', $data);
+    }
+    // public function edit_guru_tanpafoto($id,$data){
             
-            $this->db->update('tb_guru', $data);
-    }
-    public function edit_guru_tanpafoto($id){
-        $nama_guru 		= $this->input->post('nama_guru', TRUE);
-            $nip 			= $this->input->post('nip', TRUE);
-            $tempat_lahir 	= $this->input->post('tempat_lahir', TRUE);
-            $tgl_lahir 		= $this->input->post('tgl_lahir', TRUE);
-            $id_jabatan 	= $this->input->post('id_jabatan', TRUE);
-            $pendidikan 	= $this->input->post('pendidikan', TRUE);
-            $data = array(
-            	'nama_guru' => $nama_guru,
-            	'nip' => $nip,
-            	'tempat_lahir' => $tempat_lahir,
-            	'tgl_lahir' => $tgl_lahir,
-            	'id_jabatan' => $id_jabatan,
-            	'pendidikan' => $pendidikan,
-            	
-            );
-            $this->db->where('id_guru',$id);
-            $this->db->update('tb_guru', $data);
+    //         $this->db->where('id_guru',$id);
+    //         $this->db->update('tb_guru', $data);
 
-    }
+    // }
 
     
 
