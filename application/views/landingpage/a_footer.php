3ce12d362f94a6aeef932bb5e00c47799ac107a8
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-17 09:58:40 +0700

    Revert "landing page"
    This reverts commit ee1523e62523c36abf2044b6346f62b18e488231.

--- application/views/landingpage/a_footer.php
+++ application/views/landingpage/a_footer.php
@@ -48,9 +48,6 @@
     <p>Copyright © 2021 | MI Muhammadiyah Kalipepe. All rights reserved.</p>
   </div>  
 <script src="<?php echo base_url('assets/main/js/bootstrap.js') ?>"></script>
-
-<script src="assets/main/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/main/vendor/swiper/swiper-bundle.min.js"></script>
-<script src="assets/main/vendor/glightbox/js/glightbox.min.js"></script>
-
-<script src="assets/main/main.js"></script>
+<script src="<?php echo base_url('assets/main/main.js') ?>"></script>
+<script src="<?php echo base_url('assets/main/vendor/swiper/swiper-bundle.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/main/vendor/glightbox/js/glightbox.min.js') ?>"></script>
