28889635f342e48056cb47f6be3466738a2c09a4
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-17 10:05:44 +0700

    landingpage

--- application/views/landingpage/a_footer.php
+++ application/views/landingpage/a_footer.php
@@ -48,6 +48,7 @@
     <p>Copyright © 2021 | MI Muhammadiyah Kalipepe. All rights reserved.</p>
   </div>  
 <script src="<?php echo base_url('assets/main/js/bootstrap.js') ?>"></script>
-<script src="<?php echo base_url('assets/main/main.js') ?>"></script>
 <script src="<?php echo base_url('assets/main/vendor/swiper/swiper-bundle.min.js') ?>"></script>
 <script src="<?php echo base_url('assets/main/vendor/glightbox/js/glightbox.min.js') ?>"></script>
+
+<script src="<?php echo base_url('assets/main/main.js') ?>"></script>
--- application/views/landingpage/a_head.php
+++ application/views/landingpage/a_head.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
   <title>MI MUHAMMADIYAH KALIPEPE</title>
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/main/vendor/bootstrap/css/bootstrap.min.css') ?>">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/main/style.css') ?>">
-
-  <link href="<?php echo base_url('assets/main/vendor/animate.css/animate.min.css') ?>" rel="stylesheet">
-  <link href="<?php echo base_url('assets/main/vendor/fontawesome/css/all.css') ?>" rel="stylesheet">
-  <link href="<?php echo base_url('assets/main/vendor/swiper/swiper-bundle.min.css') ?>" rel="stylesheet">
-  <link href="<?php echo base_url('assets/main/vendor/glightbox/css/glightbox.min.css') ?>" rel="stylesheet">
+  
+  <link href="assets/main/vendor/fontawesome/css/all.css" rel="stylesheet">
+  <link href="assets/main/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/main/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/main/vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/main/style.css">
 </head>
 <body>
