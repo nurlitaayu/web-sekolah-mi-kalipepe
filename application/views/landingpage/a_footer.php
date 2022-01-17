ee1523e62523c36abf2044b6346f62b18e488231
Firdan Mahendra Wardana <firdanmahendraw@gmail.com>
2022-01-17 09:38:29 +0700

    landing page

--- application/views/landingpage/a_footer.php
+++ application/views/landingpage/a_footer.php
@@ -48,6 +48,9 @@
     <p>Copyright © 2021 | MI Muhammadiyah Kalipepe. All rights reserved.</p>
   </div>  
 <script src="<?php echo base_url('assets/main/js/bootstrap.js') ?>"></script>
-<script src="<?php echo base_url('assets/main/main.js') ?>"></script>
-<script src="<?php echo base_url('assets/main/vendor/swiper/swiper-bundle.min.js') ?>"></script>
-<script src="<?php echo base_url('assets/main/vendor/glightbox/js/glightbox.min.js') ?>"></script>
+
+<script src="assets/main/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/main/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/main/vendor/glightbox/js/glightbox.min.js"></script>
+
+<script src="assets/main/main.js"></script>
